0621b3cdc8204308ea31a962de084c43685c2331
Edson_Gomes <eg_f1@hotmail.com>
2019-05-27 21:43:20 -0300

    Mix Burguer Admin App V0.1.1

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:mix_burguer_admin/blocs/login_block.dart';
 import 'package:mix_burguer_admin/models/user_model.dart';
+import 'package:mix_burguer_admin/widgets/input_field.dart';
 import 'package:scoped_model/scoped_model.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -8,152 +10,73 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
-  final _formKey = GlobalKey<FormState>();
-  final _emailController = TextEditingController();
-  final _passController = TextEditingController();
+  final _loginBloc = LoginBloc();
 
-  final _scaffoldKey = GlobalKey<ScaffoldState>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        key: _scaffoldKey,
         appBar: AppBar(
           actionsIconTheme: IconThemeData(color: Colors.green),
           title: Text("MIX BURGUER"),
           centerTitle: true,
         ),
-        body: ScopedModelDescendant<UserModel>(
-          builder: (context, child, model) {
-            if (model.isLoading)
-              return Center(
-                child: CircularProgressIndicator(),
-              );
-            return Form(
-
-              key: _formKey,
-              child: ListView(
-                padding: EdgeInsets.all(16.0),
-                children: <Widget>[
-                  SizedBox(
-                    height: 25.0,
-                  ),
-                  Container(
-                    height: 200.0,
-                    child: Image.asset(
-                      'assets/mix_burguer_admin.png',
+        body:Stack(
+          alignment: Alignment.center,
+          children: <Widget>[
+            Container(),
+            SingleChildScrollView(
+              child: Container(
+                margin: EdgeInsets.all(16),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: <Widget>[
+                    Container(
+                      height: 200,
+                      alignment: Alignment.center,
+                      child: Image.asset("assets/mix_burguer_admin.png"),
                     ),
-                  ),
-                  SizedBox(
-                    height: 50.0,
-                  ),
-
-                  TextFormField(
-                    controller: _emailController,
-                    validator: (text) {
-                      if (text.isEmpty || !text.contains("@")) {
-                        return "E-mail invalido!";
+                    InputField(
+                      icon: Icons.person_outline,
+                      hint: "Usuário",
+                      obscure: false,
+                      stream: _loginBloc.outEmail,
+                      onChanged: _loginBloc.changeEmail,
+                    ),
+                    InputField(
+                      icon: Icons.lock_outline,
+                      hint: "Senha",
+                      obscure: true,
+                      stream: _loginBloc.outPassword,
+                      onChanged: _loginBloc.changePassword,
+                    ),
+                    SizedBox(
+                      height: 32,
+                    ),
+                    StreamBuilder<bool>(
+                      stream: _loginBloc.outSubmitValid,
+                      builder: (context, snapshot) {
+                        return SizedBox(
+                          height: 50,
+                          child: RaisedButton(
+                            onPressed: snapshot.hasData ? _loginBloc.submit: null,
+                            color: Colors.red,
+                            textColor: Colors.white,
+                            child: Text("Entrar"),
+                            disabledColor: Colors.grey,
+                          ),
+                        );
                       }
-                    },
-                    decoration: InputDecoration(hintText: "E-mail"),
-                    keyboardType: TextInputType.emailAddress,
-                  ),
-                  SizedBox(
-                    height: 16.0,
-                  ),
-                  TextFormField(
-                    controller: _passController,
-                    validator: (text) {
-                      if (text.isEmpty || text.length < 6) {
-                        return "Senha invalida!";
-                      }
-                    },
-                    decoration: InputDecoration(hintText: "Senha"),
-                    obscureText: true,
-                    keyboardType: TextInputType.text,
-                  ),
-                  Align(
-                    alignment: Alignment.centerRight,
-                    child: FlatButton(
-                      onPressed: () {
-                        if (_emailController.text.isEmpty)
-                          _scaffoldKey.currentState.showSnackBar(
-                            SnackBar(
-                              content: Text(
-                                  "Informe seu e-mail para recuperar sua senha!"),
-                              backgroundColor: Colors.red,
-                              duration: Duration(seconds: 2),
-                            ),
-                          );
-                        else {
-                          if(_emailController.text.contains("@") && _emailController.text.contains(".")){
-                            model.recoverPass(_emailController.text);
-                            _scaffoldKey.currentState.showSnackBar(
-                              SnackBar(
-                                content: Text("Confira sua caixa de e-mail"),
-                                backgroundColor: Colors.blue,
-                                duration: Duration(seconds: 2),
-                              ),
-                            );
-                          }else{
-                            _scaffoldKey.currentState.showSnackBar(
-                                SnackBar(
-                                  content: Text("E-mail invalido!"),
-                                  backgroundColor: Colors.red,
-                                  duration: Duration(seconds: 2),
-                                )
-                            );
-                          }
-                        }
-                      },
-                      child: Text(
-                        "Esqueci minha senha",
-                        textAlign: TextAlign.right,
-                      ),
-                      padding: EdgeInsets.zero,
                     ),
-                  ),
-                  SizedBox(
-                    height: 16.0,
-                  ),
-                  SizedBox(
-                    height: 44.0,
-                    child: RaisedButton(
-                      child: Text(
-                        "Entrar",
-                        style: TextStyle(fontSize: 18),
-                      ),
-                      textColor: Colors.white,
-                      color: Theme.of(context).primaryColor,
-                      onPressed: () {
 
-                        if (_formKey.currentState.validate()) {}
-                        model.singIn(
-                            email: _emailController.text,
-                            pass: _passController.text,
-                            onSuccess: _onSuccess,
-                            onFail: _onFail);
-                      },
-                    ),
-                  )
-                ],
+                  ],
+                ),
               ),
-            );
-          },
-        ));
-  }
+            ),
+          ],
 
-  void _onSuccess() {
-    Navigator.of(context).pop();
-  }
-
-  void _onFail() {
-    _scaffoldKey.currentState.showSnackBar(
-      SnackBar(
-        content: Text("Falha ao entrar!"),
-        backgroundColor: Colors.red,
-        duration: Duration(seconds: 2),
-      ),
+        ),
     );
   }
+
 }
---
+++ lib/validators/login_validators.dart
@@ -0,0 +1,28 @@
+import 'dart:async';
+
+class LoginValidators{
+  final validateEmail = StreamTransformer<String,String>.fromHandlers(
+    handleData: (email,sink){
+      if(email.contains("@")){
+        sink.add(email);
+      }else{
+        sink.addError("Insira um e-mail valido!");
+        
+      }
+    }
+  );
+
+  final validatePassword = StreamTransformer<String,String>.fromHandlers(
+    handleData: (password,sink){
+      if(password.length > 4){
+        sink.add(password);
+      }else{
+        sink.addError("A senha é muito curta!");
+      }
+    }
+  );
+
+
+
+
+}
