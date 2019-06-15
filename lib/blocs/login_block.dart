8e559924f34621f6ec3a0a6d4166ae52c608143f
Edson_Gomes <eg_f1@hotmail.com>
2019-06-14 21:03:34 -0300

    Mix Burguer Admin App V0.1.0

--- lib/blocs/login_block.dart
+++ lib/blocs/login_block.dart
@@ -1,9 +1,12 @@
+import 'dart:async';
+
 import 'package:bloc_pattern/bloc_pattern.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:mix_burguer_admin/validators/login_validators.dart';
 import 'package:rxdart/rxdart.dart';
 
-enum LoginState {IDLE,LOADING,SUCCESS,FAIL}
+enum LoginState { IDLE, LOADING, SUCCESS, FAIL }
 
 class LoginBloc extends BlocBase with LoginValidators {
   final _emailController = BehaviorSubject<String>();
@@ -11,50 +14,72 @@ class LoginBloc extends BlocBase with LoginValidators {
 
   final _stateController = BehaviorSubject<LoginState>();
 
-   Stream<String> get outEmail => _emailController.stream.transform(validateEmail);
-   Stream<String> get outPassword => _passwordController.stream.transform(validatePassword);
-   Stream<LoginState> get outState=> _stateController.stream;
+  Stream<String> get outEmail =>
+      _emailController.stream.transform(validateEmail);
 
-   Stream<bool> get outSubmitValid => Observable.combineLatest2(outEmail, outPassword, (a,b)=> true);
+  Stream<String> get outPassword =>
+      _passwordController.stream.transform(validatePassword);
 
+  Stream<LoginState> get outState => _stateController.stream;
 
-   Function(String) get changeEmail =>_emailController.sink.add;
-   Function(String) get changePassword =>_passwordController.sink.add;
+  Stream<bool> get outSubmitValid =>
+      Observable.combineLatest2(outEmail, outPassword, (a, b) => true);
 
-  LoginBloc(){
-    FirebaseAuth.instance.onAuthStateChanged.listen((user){
-      if(user != null){
-        print("ok Logou !!!!!!!!!!");
-        FirebaseAuth.instance.signOut();
+  Function(String) get changeEmail => _emailController.sink.add;
 
-      }else{
-        print("não Logou !!!!!!!!!!");
+  Function(String) get changePassword => _passwordController.sink.add;
+
+StreamSubscription _streamSubscription;
+
+  LoginBloc() {
+    _streamSubscription = FirebaseAuth.instance.onAuthStateChanged.listen((user) async {
+      if (user != null) {
+        if (await verifyPrivileges(user)) {
+          _stateController.add(LoginState.SUCCESS);
+        } else {
+          FirebaseAuth.instance.signOut();
+          _stateController.add(LoginState.FAIL);
+
+        }
+      } else {
         _stateController.add(LoginState.IDLE);
-
       }
-
     });
   }
 
-   void submit(){
-     final email = _emailController.value;
-     final password = _passwordController.value;
-     
-     _stateController.add(LoginState.LOADING);
-     FirebaseAuth.instance.signInWithEmailAndPassword(email: email, password: password)
-         .catchError((e){
-           _stateController.add(LoginState.FAIL);
-     });
+  void submit() {
+    final email = _emailController.value;
+    final password = _passwordController.value;
 
+    _stateController.add(LoginState.LOADING);
+    FirebaseAuth.instance
+        .signInWithEmailAndPassword(email: email, password: password)
+        .catchError((e) {
+      _stateController.add(LoginState.FAIL);
+    });
+  }
 
-   }
+  Future<bool> verifyPrivileges(FirebaseUser user) async {
+    return await Firestore.instance
+        .collection("admins")
+        .document(user.uid)
+        .get()
+        .then((doc) {
+      if (doc.data != null) {
+        return true;
+      } else {
+        return false;
+      }
+    }).catchError((e) {
+      return false;
+    });
+  }
 
   @override
   void dispose() {
-
     _emailController.close();
     _passwordController.close();
     _stateController.close();
+    _streamSubscription.cancel();
   }
-
 }
--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:mix_burguer_admin/blocs/login_block.dart';
-import 'package:mix_burguer_admin/models/user_model.dart';
 import 'package:mix_burguer_admin/widgets/input_field.dart';
-import 'package:scoped_model/scoped_model.dart';
+
+import 'home_screen.dart';
 
 class LoginScreen extends StatefulWidget {
   @override
@@ -14,69 +14,109 @@ class _LoginScreenState extends State<LoginScreen> {
 
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          actionsIconTheme: IconThemeData(color: Colors.green),
-          title: Text("MIX BURGUER"),
-          centerTitle: true,
-        ),
-        body:Stack(
-          alignment: Alignment.center,
-          children: <Widget>[
-            Container(),
-            SingleChildScrollView(
-              child: Container(
-                margin: EdgeInsets.all(16),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: <Widget>[
-                    Container(
-                      height: 200,
-                      alignment: Alignment.center,
-                      child: Image.asset("assets/mix_burguer_admin.png"),
-                    ),
-                    InputField(
-                      icon: Icons.person_outline,
-                      hint: "Usuário",
-                      obscure: false,
-                      stream: _loginBloc.outEmail,
-                      onChanged: _loginBloc.changeEmail,
-                    ),
-                    InputField(
-                      icon: Icons.lock_outline,
-                      hint: "Senha",
-                      obscure: true,
-                      stream: _loginBloc.outPassword,
-                      onChanged: _loginBloc.changePassword,
-                    ),
-                    SizedBox(
-                      height: 32,
-                    ),
-                    StreamBuilder<bool>(
-                      stream: _loginBloc.outSubmitValid,
-                      builder: (context, snapshot) {
-                        return SizedBox(
-                          height: 50,
-                          child: RaisedButton(
-                            onPressed: snapshot.hasData ? _loginBloc.submit: null,
-                            color: Colors.red,
-                            textColor: Colors.white,
-                            child: Text("Entrar"),
-                            disabledColor: Colors.grey,
-                          ),
-                        );
-                      }
-                    ),
+  void initState() {
+    super.initState();
+    _loginBloc.outState.listen((state){
+      switch(state){
+        case LoginState.SUCCESS:
+          Navigator.of(context).pushReplacement(
+              MaterialPageRoute(builder: (context)=>HomeScreen())
+          );
+          break;
+        case LoginState.FAIL:
+          showDialog(context: context,builder: (context)=>AlertDialog(
+            title: Text("Erro"),
+            content: Text("Você não posssui privilegios necessários!"),
+          ));
+          break;
+        case LoginState.LOADING:
+        case LoginState.IDLE:
 
-                  ],
-                ),
-              ),
-            ),
-          ],
+      }
 
-        ),
-    );
+    });
+
   }
 
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        actionsIconTheme: IconThemeData(color: Colors.green),
+        title: Text("MIX BURGUER"),
+        centerTitle: true,
+      ),
+      body: StreamBuilder<LoginState>(
+          stream: _loginBloc.outState,
+          initialData: LoginState.LOADING,
+          builder: (context, snapshot) {
+            switch (snapshot.data) {
+              case LoginState.LOADING:
+                return Center(
+                  child: CircularProgressIndicator(
+                    valueColor: AlwaysStoppedAnimation(Colors.white),
+                  ),
+                );
+              case LoginState.FAIL:
+              case LoginState.SUCCESS:
+              case LoginState.IDLE:
+                return Stack(
+                  alignment: Alignment.center,
+                  children: <Widget>[
+                    Container(),
+                    SingleChildScrollView(
+                      child: Container(
+                        margin: EdgeInsets.all(16),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          children: <Widget>[
+                            Container(
+                              height: 200,
+                              alignment: Alignment.center,
+                              child:
+                                  Image.asset("assets/mix_burguer_admin.png"),
+                            ),
+                            InputField(
+                              icon: Icons.person_outline,
+                              hint: "Usuário",
+                              obscure: false,
+                              stream: _loginBloc.outEmail,
+                              onChanged: _loginBloc.changeEmail,
+                            ),
+                            InputField(
+                              icon: Icons.lock_outline,
+                              hint: "Senha",
+                              obscure: true,
+                              stream: _loginBloc.outPassword,
+                              onChanged: _loginBloc.changePassword,
+                            ),
+                            SizedBox(
+                              height: 32,
+                            ),
+                            StreamBuilder<bool>(
+                                stream: _loginBloc.outSubmitValid,
+                                builder: (context, snapshot) {
+                                  return SizedBox(
+                                    height: 50,
+                                    child: RaisedButton(
+                                      onPressed: snapshot.hasData
+                                          ? _loginBloc.submit
+                                          : null,
+                                      color: Colors.red,
+                                      textColor: Colors.white,
+                                      child: Text("Entrar"),
+                                      disabledColor: Colors.grey,
+                                    ),
+                                  );
+                                }),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                );
+            }
+          }),
+    );
+  }
 }
